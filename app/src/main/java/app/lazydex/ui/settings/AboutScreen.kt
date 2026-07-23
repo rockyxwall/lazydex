75b7a2bd5c95032977b3118e5e4ca1f6bf1aafdb
rockyxwall <tanvird556@gmail.com>
2026-07-23 21:02:40 +0600

    fix(settings): change contradictory 'Stable v0.0.1-beta' text to 'Version 0.0.1-beta' in AboutScreen

--- app/src/main/java/app/lazydex/ui/settings/AboutScreen.kt
+++ app/src/main/java/app/lazydex/ui/settings/AboutScreen.kt
@@ -136,7 +136,7 @@ fun AboutScreen(
                 color = MaterialTheme.colorScheme.onBackground
             )
             Text(
-                text = "Stable v${BuildConfig.VERSION_NAME}",
+                text = "Version ${BuildConfig.VERSION_NAME}",
                 fontSize = 13.sp,
                 color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.6f)
             )
